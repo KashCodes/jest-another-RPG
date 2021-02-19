854687fd73fb6807fa7ae9da013eb7487d99876d
Kashay Arbelo <kashay@agyeman.me>
2021-02-19 11:21:32 -0700

    updated Player.js to pull player stats using prototypes versus this.methodname

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,17 +1,50 @@
+//Tells the page it requires the Player.js constructor functions to run test. 
 const Player = require('../lib/Player');
 
+// Pulls from __mocks__ folder instead for this Potion file. In it is a specific potion. 
 jest.mock('../lib/Potion');
 
-// console.log(new Potion());
+        /*console.log(new Potion()); ----- Does not work - should be looking for a new potion object, which should include a name and value per Potion.js*/
 
+//Tests play constructor function to ensure it creates a player object with a name and 4 additional properties. 
 test('creates a player object', () => {
+  // `new` always creates new player object. 
   const player = new Player('Dave');
 
+  // name will always be dave
   expect(player.name).toBe('Dave');
+  // health is equal to any number
   expect(player.health).toEqual(expect.any(Number));
+  // strength is equal to any number
   expect(player.strength).toEqual(expect.any(Number));
+  // agility is equal to any number
   expect(player.agility).toEqual(expect.any(Number));
+  // inventory is expected to be an array with any objects specified.
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
+});
+
+// Here, we're checking that player.getStats() returns an object with four specific properties.
+test("gets player's stats as an object", () => {
+  const player = new Player('Dave');
+
+  // all of these are checking to ensure the 4 properties are created and have stats assigned.
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+
+
+//The getInventory() method will return an array of Potion objects or return false if the inventory is empty.
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,16 +1,25 @@
+////Tells the page it requires the Potion.js constructor functions to run test. 
 const Potion = require('../lib/Potion.js');
 
+// Tests to ensure constuctor function creates a health potion with a name and a value. 
 test('creates a health potion object', () => {
+  // `new` ensures it is always a new object, this one is specifically a health potion.
   const potion = new Potion('health');
 
+  // potion created has to be named health.
   expect(potion.name).toBe('health');
+  // value assign can be any number.
   expect(potion.value).toEqual(expect.any(Number));
 });
 
+// Tests to ensure constuctor function creates a random potion object other than a health potion.  
 test('creates a random potion object', () => {
   const potion = new Potion();
 
+  // 3 potions are in the string, must equal one of them. 
   expect(potion.name).toEqual(expect.any(String));
+  //This can have any amount of potions as long as there is more than 0.
   expect(potion.name.length).toBeGreaterThan(0);
+  // the potion value you be equal to any number. 
   expect(potion.value).toEqual(expect.any(Number));
 });
